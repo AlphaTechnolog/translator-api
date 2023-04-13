8e92c24adda5de9a5b448d8e189377055c0264b0
Gabriel Guerra <gfranklings@gmail.com>
2023-04-13 13:33:47 -0400

    [chore] configured user agent to be like firefox

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, request
 from dotenv import load_dotenv
 
+poe.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+
 load_dotenv()
 
 
